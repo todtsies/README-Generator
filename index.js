b525713cb8efccecf255b486ad037b9773aee88e
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-02-01 11:20:55 -0500

    Update index.js
    Added questions to prompt user, created function to initialize app, and function to write readme file

--- index.js
+++ index.js
@@ -1,17 +1,112 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create an array of questions for user input
+const questions = [
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        type: 'input',
+        default: 'Project Title',
+        message: 'What is the name of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please briefly describe your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Please describe the installation process if any.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What is this project usage for?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to this project?',
+        name: 'contributors'
+    },
+    {
+        type: 'input',
+        message: 'Were there any tests included?',
+        name: 'tests'
+    },
+    {
+        type: "list",
+        message: "Choose the appropriate license for this project: ",
+        name: "license",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: 'checkbox',
+        message: "Select all technologies used in building the application:",
+        name: 'technolgies',
+        choices: [
+            "HTML", 
+            "CSS", 
+            "Bootstrap 4", 
+            "JavaScript", 
+            "jQuery", 
+            "React.js", 
+            "Node.js", 
+            "Express", 
+            "MySQL", 
+            "MongoDb"
+        ]
+      },
+    {
+        type: 'input',
+        message: 'Please enter your Github username: ',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address: ',
+        name: 'email'
+    },
+];
+
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Success! The README.md file was generated!");
+    }
+)};
+
+const writeFileAsync = util.promisify(writeToFile);
+
+// Create a function to initialize app
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        const markdown = generateMarkdown(answers);
+        console.log(markdown);
+
+        await writeFileAsync('README.md', markdown);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 // Function call to initialize app
 init();
